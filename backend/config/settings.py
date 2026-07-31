5a206b0e0c641bf5921c8c8e05953d2d78877136
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-07-31 16:21:19 +0100

    Feat : add minio settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,3 +145,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+
+# minio settings 
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
+MINIO_BUCKET_NAME = "documents"
