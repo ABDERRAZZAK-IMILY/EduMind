6f50a6df5d61277bbe845e0dd781df542e85bad9
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-07-31 15:45:06 +0100

    Feat : create account model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        APPRENANT = "APPRENANT", "Apprenant"
+        ADMINISTRATEUR = "ADMINISTRATEUR", "Administrateur"
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.APPRENANT)
+    max_documents = models.PositiveIntegerField(default=10)
+    max_storage_mb = models.PositiveIntegerField(default=500)
