959ac05ef8d53eebc850cb08dcc67ce62528fe33
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-08-02 18:16:16 +0100

    Feat : update docment urls

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RequestUploadUrlView
+from .views import RequestUploadUrlView , ConfirmUploadView
 
 urlpatterns = [
     path("upload-url/", RequestUploadUrlView.as_view(), name="request-upload-url"),
+    path("upload-url/", RequestUploadUrlView.as_view(), name="request-upload-url"),
+    path("<int:document_id>/confirm-upload/", ConfirmUploadView.as_view(), name="confirm-upload"),
 ]
