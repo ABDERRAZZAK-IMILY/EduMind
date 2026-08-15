4641677a614f1fcc45d674c5bf4231cd1e8439bc
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-08-15 13:26:03 +0100

    update urls for listquze

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GenerateQuizView, SubmitQuizView
+from .views import GenerateQuizView, SubmitQuizView, QuizListView
 
 
 urlpatterns = [
+    path("", QuizListView.as_view(), name="quiz-list"),
     path("generate/", GenerateQuizView.as_view(), name="generate-quiz"),
     path("<int:quiz_id>/submit/", SubmitQuizView.as_view(), name="submit-quiz"),
 ]
