4ffc5117aa74acb3de3886cd0ee2b3a66ec9ebe7
ABDERRAZZAK IMILY <imily2024@gmail.com>
2026-08-15 16:24:23 +0100

    update uurls

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import RequestUploadUrlView , ConfirmUploadView
+from .views import (
+    DocumentListView,
+    RequestUploadUrlView,
+    ConfirmUploadView,
+    DocumentDetailView,
+)
 
 urlpatterns = [
-    path("upload-url/", RequestUploadUrlView.as_view(), name="request-upload-url"),
+    path("", DocumentListView.as_view(), name="document-list"),
     path("upload-url/", RequestUploadUrlView.as_view(), name="request-upload-url"),
     path("<int:document_id>/confirm-upload/", ConfirmUploadView.as_view(), name="confirm-upload"),
+    path("<int:document_id>/", DocumentDetailView.as_view(), name="document-detail"),
 ]
